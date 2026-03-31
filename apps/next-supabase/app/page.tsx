95c4ad78af20cf76c2c084abaad3d2c59569f513
황상윤 <s_infinite@naver.com>
2026-04-01 01:28:04 +0900

    [feat] main layout 수정

--- apps/next-supabase/app/page.tsx
+++ apps/next-supabase/app/page.tsx
@@ -1,8 +1,15 @@
+import LogoutButton from "@/components/logout-button";
+
 export const metadata = {
   title: "Clonegram",
   description: "Instagram clone project",
 };
 
 export default function Home() {
-  return <main className="">Clonegram</main>;
+  return <main className="w-full h-screen flex items-center justify-center flex-col gap-2">
+    <h1 className="text-3xl font-bold">
+      Welcome {"lopun.jh"}
+    </h1>
+    <LogoutButton />
+  </main>;
 }
--- apps/next-supabase/components/layouts/main-layout.tsx
+++ apps/next-supabase/components/layouts/main-layout.tsx
@@ -2,7 +2,7 @@ import SiderBar from "../sidebar";
 
 export default async function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <main className="">
+    <main className="w-full h-screen flex items-center justify-center">
       <SiderBar />
       {children}
     </main>
