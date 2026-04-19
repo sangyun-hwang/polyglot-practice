e9dbfcf95db25c3335d948f6b54d843aa6b523e4
sangyun-hwang <s_infinite@naver.com>
2026-04-19 22:36:24 +0900

    [feat] type 수정

--- apps/next-supabase/config/auth-provider.tsx
+++ apps/next-supabase/config/auth-provider.tsx
@@ -4,7 +4,7 @@ import { usePathname, useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { createBrowserSupabaseClient } from "@/utils/supabase/client";
 
-export default function AuthProvider({ accessToken, children }) {
+export default function AuthProvider({ accessToken, children }: { accessToken?: string | null; children: React.ReactNode }) {
   const supabase = createBrowserSupabaseClient();
   const router = useRouter();
 
